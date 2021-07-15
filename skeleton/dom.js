d4012bb571f82d9a53dccd60cdb95835b324ce4f
LinaYahya <linaebe0@gmail.com>
2021-07-15 14:24:56 +0300

    create files and todo functions

---
+++ skeleton/dom.js
@@ -0,0 +1,71 @@
+// part 2 linking it all together
+// The function here is called an iife,
+// it keeps everything inside hidden from the rest of our application
+(function () {
+    // This is the dom node where we will keep our todo
+    const container = document.getElementById("todo-container");
+    const addTodoForm = document.getElementById("add-todo");
+
+    let state = [
+        { id: -3, description: "first todo" },
+        { id: -2, description: "second todo" },
+        { id: -1, description: "third todo" },
+    ]; // this is our initial todoList
+
+    // This function takes a todo, it returns the DOM node representing that todo
+    const createTodoNode = function (todo) {
+        const todoNode = document.createElement("li");
+        // you will need to use addEventListener
+
+        // add span holding description
+
+        // this adds the delete button
+        const deleteButtonNode = document.createElement("button");
+        deleteButtonNode.addEventListener("click", function (event) {
+            const newState = todoFunctions.deleteTodo(state, todo.id);
+            update(newState);
+        });
+        todoNode.appendChild(deleteButtonNode);
+
+        // add markTodo button
+
+        // add classes for css
+
+        return todoNode;
+    };
+
+    // bind create todo form
+    if (addTodoForm) {
+        addTodoForm.addEventListener("submit", function (event) {
+            // https://developer.mozilla.org/en-US/docs/Web/Events/submit
+            // what does event.preventDefault do?
+            // what is inside event.target?
+
+            const description = "?"; // event.target ....
+
+            // hint: todoFunctions.addTodo
+            const newState = []; // ?? change this!
+            update(newState);
+        });
+    }
+
+    // you should not need to change this function
+    const update = function (newState) {
+        state = newState;
+        renderState(state);
+    };
+
+    // you do not need to change this function
+    const renderState = function (state) {
+        const todoListNode = document.createElement("ul");
+
+        state.forEach(function (todo) {
+            todoListNode.appendChild(createTodoNode(todo));
+        });
+
+        // you may want to add a class for css
+        container.replaceChild(todoListNode, container.firstChild);
+    };
+
+    if (container) renderState(state);
+})();
---
+++ skeleton/test.js
@@ -0,0 +1,5 @@
+const logic = require("./logic");
+
+test("Testing Jest is working", function () {
+    expect(2 + 2).toBe(4);
+});
